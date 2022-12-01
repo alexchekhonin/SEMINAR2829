b94fdf03905e705146b3c8500b92c857839dd1c5
Александр Чехонин <2007cheh@mail.ru>
2022-12-01 13:03:36 +0300

    Треуг паскаля нечетные

--- TreugPascal/Program.cs
+++ TreugPascal/Program.cs
@@ -1,45 +1,47 @@
 ﻿// Треугольник Паскаля
 int[,] CreateTriangle(int row)
 {
-  int[,] triangle = new int[row, row];
-  for (int i = 0; i < row; i++)
-  {
-    triangle[i, 0] = 1;
-    triangle[i, i] = 1;
-  }
-
-  for (int i = 2; i < row; i++)
-  {
-    for (int j = 1; j <= i; j++)
+    int[,] triangle = new int[row, row];
+    for (int i = 0; i < row; i++)
     {
-      triangle[i, j] =
-      triangle[i - 1, j - 1] + triangle[i - 1, j];
+        triangle[i, 0] = 1;
+        triangle[i, i] = 1;
     }
-  }
-  return triangle;
+
+    for (int i = 2; i < row; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            triangle[i, j] =
+            triangle[i - 1, j - 1] + triangle[i - 1, j];
+        }
+    }
+    return triangle;
 }
 
 void PrintTriangle(int[,] triangle)
 {
-  int row = triangle.GetLength(0);
-  for (int i = 0; i < row; i++)
-  {
-    for (int j = 0; j < row; j++)
+    int row = triangle.GetLength(0);
+    for (int i = 0; i < row; i++)
     {
-      if (triangle[i, j] != 0) Console.Write($"{triangle[i, j],4}");
+        for (int j = 0; j < row; j++)
+        {
+            if (triangle[i, j] != 0)
+                if (triangle[i, j] % 2 != 0) Console.Write($"{triangle[i, j],4}");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
     }
-    Console.WriteLine();
-  }
 }
 
 int[] GetKoeff(int[,] tr, int pow)
 {
-  int[] row = new int[pow + 1];
-  for (int i = 0; i <= pow; i++)
-  {
-    row[i] = tr[pow, i];
-  }
-  return row;
+    int[] row = new int[pow + 1];
+    for (int i = 0; i <= pow; i++)
+    {
+        row[i] = tr[pow, i];
+    }
+    return row;
 }
 
 int[,] tr = CreateTriangle(10);
@@ -50,5 +52,5 @@ System.Console.WriteLine(String.Join(' ', koeff));
 
 for (int i = 0; i < koeff.Length; i++)
 {
-  Console.Write($"{koeff[i]}*a^{koeff.Length - i}*b^{i} + ");
+    Console.Write($"{koeff[i]}*a^{koeff.Length - i}*b^{i} + ");
 }
